9b1ba06cf1c0aa8f940caa5e9710f79337e87861
Tyler Mercado <tyler_mercado@hotmail.com>
2024-02-15 15:12:13 -0700

    Changed both models from a HABTM association to a 'has_many :through' association for more flexibility with how I use the join table.

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,3 +1,5 @@
 class Pizza < ApplicationRecord
-	has_and_belongs_to_many :toppings, join_table: 'pizzas_toppings'
+	# has_and_belongs_to_many :toppings, join_table: 'pizzas_toppings'
+	has_many :pizza_toppings
+	has_many :toppings, through: :pizza_toppings
 end
--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,3 +1,5 @@
 class Topping < ApplicationRecord
-	has_and_belongs_to_many :pizzas, join_table: 'pizzas_toppings'
+	# has_and_belongs_to_many :pizzas, join_table: 'pizzas_toppings'
+	has_many :pizza_toppings
+	has_many :pizzas, through: :pizza_toppings
 end
