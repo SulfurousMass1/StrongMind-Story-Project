51f80243cf2543900c6e8f5d03942a18a802a96f
Tyler Mercado <tyler_mercado@hotmail.com>
2024-02-15 16:18:04 -0700

    Added 'dependent: :destroy' after the 'has_many :pizza_toppings' association so that it could destroy all associated PizzaTopping records when trying to delete a pizza.

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,5 +1,5 @@
 class Pizza < ApplicationRecord
 	# has_and_belongs_to_many :toppings, join_table: 'pizzas_toppings'
-	has_many :pizza_toppings
+	has_many :pizza_toppings, dependent: :destroy
 	has_many :toppings, through: :pizza_toppings
 end
