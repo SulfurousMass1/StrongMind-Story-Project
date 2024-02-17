cb34d51c7eb25b4ced4b14f174ec55f1fc19b0bd
Tyler Mercado <tyler_mercado@hotmail.com>
2024-02-17 00:23:13 -0700

    Created the current_toppings variable so that I could easily compare similar pizzas to the current pizza. Changed the attribute being plucked in line 31 from :id to :topping_name as debugging helped me realize that trying to previously sort by IDs was impossible since the validation is occurring before anything is being saved into the database table. By creating the current_toppings variable, I was able to map the toppings selected to a variable that could be used while being validated. Also commented out all the debugging loggers because I want to keep them as they were extremely useful in helping me realize the issue and find a solution.

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -13,7 +13,7 @@ class Pizza < ApplicationRecord
 		
 		# Creates an array of all the current toppings of the pizza being created/updated. Necessary for comparing similar pizzas
 		# because the current pizza cannot use .pluck(:topping) since the validation occurs before it is saved in the database
-		current_toppings = toppings.map(&:topping).sort
+		current_toppings = toppings.map(&:topping_name).sort
 
 		# Find pizzas with the same crust
 		similar_pizzas = Pizza.where(crust: crust)
@@ -22,13 +22,13 @@ class Pizza < ApplicationRecord
 		similar_pizzas = similar_pizzas.where.not(id: id) if persisted?
 
 		similar_pizzas.each do |pizza|
-			Rails.logger.debug "--------------------------START OF LOOP----------------------------"
-			Rails.logger.debug "CURRENT PIZZA TOPPINGS: #{current_toppings}"
-			Rails.logger.debug "SIMILAR PIZZA: #{pizza.toppings.pluck(:topping).sort}"
+			# Rails.logger.debug "--------------------------START OF LOOP----------------------------"
+			# Rails.logger.debug "CURRENT PIZZA TOPPINGS: #{current_toppings}"
+			# Rails.logger.debug "SIMILAR PIZZA: #{pizza.toppings.pluck(:topping_name).sort}"
 			
-			# Compares toppings between the current pizza (toppings.pluck(:id).sort) and similar pizzas (pizza.toppings.pluck(:id).sort)
-			# Sorting ensures that the IDs are more accurately compared
-			if pizza.toppings.pluck(:topping).sort == current_toppings
+			# Compares toppings between the current pizza (current_toppings) and similar pizzas (pizza.toppings.pluck(:topping_name).sort)
+			# Sorting ensures that the toppings are more accurately compared
+			if pizza.toppings.pluck(:topping_name).sort == current_toppings
 				errors.add(:base, "A pizza with the same crust and toppings you specified already exists")
 				Rails.logger.debug "-------------------------BREAK-------------------------------"
 				break
