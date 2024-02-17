36f948c31ab61d2f9111959ad188043728b08926
Tyler Mercado <tyler_mercado@hotmail.com>
2024-02-17 01:19:14 -0700

    Added a case insensitive unique validation for topping_name

--- app/models/topping.rb
+++ app/models/topping.rb
@@ -1,4 +1,6 @@
 class Topping < ApplicationRecord
+	validates :topping_name, uniqueness: {case_sensitive: false}
+
 	# has_and_belongs_to_many :pizzas, join_table: 'pizzas_toppings'
 	has_many :pizza_toppings
 	has_many :pizzas, through: :pizza_toppings
